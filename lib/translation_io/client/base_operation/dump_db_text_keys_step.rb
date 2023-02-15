23d628a30dd44c45882242390eedce559f7077e4
Wendy Shan <92752107+pengyin-shan@users.noreply.github.com>
2023-02-15 12:27:16 -0500

    Update dump_db_text_keys_step.rb
    test

--- lib/translation_io/client/base_operation/dump_db_text_keys_step.rb
+++ lib/translation_io/client/base_operation/dump_db_text_keys_step.rb
@@ -37,9 +37,9 @@ module TranslationIO
           entries = []
 
           @db_fields.keys.each do |table_name|
+            p "table name is " + table_name.to_s
             table = table_name.constantize
             @db_fields[table_name].each do |column_name|
-              puts "table name is " + table_name.to_s
               db_strings = if table_name == 'phases' then table.unscoped.distinct.pluck(column_name) else table.distinct.pluck(column_name) end
               entries += db_strings
             end
